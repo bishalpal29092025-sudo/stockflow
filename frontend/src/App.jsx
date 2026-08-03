3c9b77cafe0b11aeabd724349c4e3b33f46fcef1
Bishal Pal <bishalpal29092025@gmail.com>
2026-08-03 12:26:23 +0530

    fix: improve deployment and CORS configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import { Box } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CreatePage from "./pages/CreatePage";
+
 import Navbar from "@/components/layout/Navbar";
 
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import EditPage from "./pages/EditPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 function App() {
-  
   return (
-    <Box minH={"100vh"}>
-      <Navbar/>
+    <Box minH="100vh">
+      <Navbar />
+
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/create" element={<CreatePage/>}/>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/create" element={<CreatePage />} />
+
+        <Route path="/edit/:id" element={<EditPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Box>
   );
